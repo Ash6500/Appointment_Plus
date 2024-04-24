cebaeb809d5ddbb40ac823812d7abb1901dbde1d
Sareen76 <138465039+Sareen76@users.noreply.github.com>
2024-04-24 19:44:03 +0530

    done user functions

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -20,7 +20,51 @@ const addUsers = async (req, res) => {
     }
 }
 
+const getUserById = async (req, res) =>{
+    try{
+        const {id }= req.params;
+        const user = await userModel.find({"_id": id});  // Here the id is coming from routes :id so don't change the variable name hwile destructuring
+        if(user.length >0){
+            res.status(200).json(user);
+        } else {
+            res.status(404).json({message: "Record not found. Check ID and try again."});
+        }
+    } catch (error) {
+        if(error.name === "CastError"){
+            res.status(400).json({"message": "Invalid Id"});
+        } else {
+            res.status(500).json(error);
+        }
+    }
+}
+
+const updateUser = async (req, res) => {
+    try{
+        const { id } = req.params;
+
+        // Now i want to show the deatils in the inoput field for better user friendliness
+        // const beforeUpdate = await userModel.find( { "_id" : id } ); 
+        // res.status(200).json(beforeUpdate);
+        
+        // Now this one is updated
+        const user = await userModel.findOneAndUpdate({"_id" : id}, req.body, {new: true});
+        if(user){
+            res.status(200).json({message : "Updated Successfully", data : user});// with new : true the mongoose will return the new updaterd da
+        } else {
+            res.status(404).json({"message": "Record not found"})
+        }
+    }catch (error) {
+        if(error.name === "CastError"){
+            res.status(400).json({"message": "Invalid Id"})
+        } else {
+            res.status(500).json(error);
+        }
+    }
+}
+
 module.exports = {
     getUsers,
-    addUsers
+    addUsers,
+    updateUser,
+    getUserById
 }
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,12 +2,17 @@ const express = require("express");
 
 const {
     getUsers,
-    addUsers
+    addUsers,
+    updateUser,
+    getUserById
 } = require("../controller/user.controller.js");
 
 const userRouter = express.Router();
 
 userRouter.get("/", getUsers);
 userRouter.post("/", addUsers);
+userRouter.post("/:id", updateUser);
+userRouter.get("/:id", getUserById);
+
 
 module.exports = userRouter;
